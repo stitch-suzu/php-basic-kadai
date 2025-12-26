51ccb205194480af03efb15c3d10f652d3b7f5c2
stitch-suzu <suzuna1225airi727@ezweb.ne.jp>
2025-12-26 21:51:07 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,11 @@
+<?php
+ $onion_data = [
+  'name' => '玉ねぎ',
+  'price' => '200',
+  'place' => '北海道'
+ ];
+
+ foreach ($onion_data as $key => $value) {
+   echo $key . ':' . $value . '<br>';
+ }
+ ?>
